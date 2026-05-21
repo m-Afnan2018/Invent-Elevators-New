6b5bae5fa14f0123d28b0fe4588708681c9d4429
m-Afnan2018 <m.afnan2018@gmail.com>
2026-05-21 12:33:09 +0530

    Updated

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -3,6 +3,9 @@ import localFont from "next/font/local";
 import { Montserrat } from "next/font/google";
 import { Lato } from "next/font/google";
 import { Toaster } from "react-hot-toast";
+import Script from 'next/script'
+
+const GTM_ID = 'GTM-NRTCMXXT'
 
 const aeonikPro = localFont({
   src: "../assets/fonts/AeonikProTRIAL-Regular.otf",
@@ -82,7 +85,32 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${aeonikPro.variable} ${montserrat.variable} ${gotham.variable} ${verah.variable} ${aeonikMediumPro.variable}`}>
+      <head>
+        {/* GTM Added */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
+      </head>
       <body>
+      {/* GTM NoScript Fallback */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Toaster
           position="top-right"
           toastOptions={{
